a76a33a369e108d0df75824a72798e4f9054a49d
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-13 11:25:18 +0530

    added

---
+++ datatype.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string vari = "Pranay";
+    double vr = 22.333;
+    int ew = 21;
+    cout << "Hello World!" << vari << vr << ew;
+    return 0;
+}
