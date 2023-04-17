784cf5693347387f24fa58b1d7509aee9cb8e255
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-17 16:54:18 +0530

    added

--- constructor.cpp
+++ constructor.cpp
@@ -7,17 +7,16 @@ class Book {
         string title;
         string author;
         int pages;
-        Book( string nm){
+        Book( string nm, string s_title, string s_author, int pg){
             cout << "Creating object" << nm;
+            title = s_title;
+            author = s_author;
+            pages = pg;
         }
 };
 
 int main(){
-    Book book1(" Hello ");
-
-    book1.title = "harrry Potter";
-    book1.author = "jk rowling";
-    book1.pages = 500;
+    Book book1(" Hello ", "harrry Potter", "jk rowling", 500);
 
     cout << book1.title;
 
