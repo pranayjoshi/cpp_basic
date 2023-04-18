745be6b508d59252ac7dd1ad86bff49a1f47fdb8
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-18 20:25:52 +0530

    added

--- gettersSetters.cpp
+++ gettersSetters.cpp
@@ -3,15 +3,27 @@
 using namespace std;
 
 class Book {
+
+    private:
+        string author;
+
     public:
         string title;
-        string author;
+        
         int pages;
         Book( string nm, string s_title, string s_author, int pg){
             cout << "Creating object" << nm;
             title = s_title;
-            author = s_author;
+            setRating(s_author) ;
             pages = pg;
+        }
+
+        void setRating(string s_author){
+            author = s_author;
+        }
+
+        string getRating(){
+            return author;
         }
 
         
@@ -20,7 +32,9 @@ class Book {
 int main(){
     Book book1(" Hello ", "harrry Potter", "jk rowling", 500);
 
-    cout << book1.title << book1.isLengthy();
+    book1.setRating("hehe");
+
+    cout << book1.title << book1.getRating();
 
     return 0;
 }
